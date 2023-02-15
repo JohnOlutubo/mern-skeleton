954dd5205162b7e53cb79d81660271ae54fd4b87
JohnOlutubo <107564128+JohnOlutubo@users.noreply.github.com>
2023-02-15 23:15:32 +0100

    define some server-side configuration-related variables

---
+++ config/config.js
@@ -0,0 +1,22 @@
+/* 
+This defines some server-side configuration-related variables 
+that will be used in the code but should not be hardcoded 
+as a best practice, as well as for security purposes.
+ */
+
+const config = {
+  env: process.env.NODE_ENV || "development", // to differentiate between development and production environments
+  port: process.env.PORT || 3000, // to define the listening port for the server
+  jwtSecret: process.env.JWT_SECRET || "YOUR_secret_key", // The secret key to be used to sign JWT
+  // The location of MongoDB database instance for the project
+  mongoUri:
+    process.env.MONGODB_URI ||
+    process.env.MONGO_HOST ||
+    "mongodb://" +
+      (process.env.IP || "localhost") +
+      ":" +
+      (process.env.MONGO_PORT || "27017") +
+      "/mernproject",
+};
+
+export default config;
