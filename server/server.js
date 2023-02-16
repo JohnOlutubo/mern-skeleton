f755d69af4adfa7138e752c97485bbb530ee21df
JohnOlutubo <107564128+JohnOlutubo@users.noreply.github.com>
2023-02-16 16:01:57 +0100

    update server.js to import the mongoose module configure it so that it uses native ES6 promise use it to handle connection to the MongoDB database

--- server/server.js
+++ server/server.js
@@ -1,9 +1,21 @@
 import config from "./../config/config.js"; //import the variable to set the port number that the server will listen
 import app from "./express.js"; // import the configured Express app to start the server
+import mongoose from "mongoose"; // use this module to implement the user model in this skeleton
 
 app.listen(config.port, (err) => {
   if (err) {
     console.log(err);
   }
   console.info("Server started on port %s.", config.port);
+});
+
+mongoose.Promise = global.Promise;
+mongoose.connect(config.mongoUri, {
+  useNewUrlParser: true,
+  useCreateIndex: true,
+  useUnifiesTopology: true,
+});
+
+mongoose.connection.on("error", () => {
+  throw new Error(`unable to connect to database: ${mongoUri}`);
 });
