797e5746efbb91dd6657ca541a8a7de217360b57
JohnOlutubo <107564128+JohnOlutubo@users.noreply.github.com>
2023-02-18 20:00:53 +0100

    add fields for created and updated timestamps in user.models.js

--- models/user.model.js
+++ models/user.model.js
@@ -1,5 +1,6 @@
 import mongoose from "mongoose"; // use Mongoose to define the schema with the necessary user data fields
 
+//takes a schema definition object as a parameter to generate a new Mongoose schema object that will specify the properties or structure of each document in a collection.
 const UserSchema = new mongoose.Schema({
   name: {
     type: String,
@@ -13,6 +14,13 @@ const UserSchema = new mongoose.Schema({
     match: [/.+\@.+\..+/, "Please fill a valid email address"],
     required: "Email is required",
   },
-}); //takes a schema definition object as a parameter to generate a new Mongoose schema object that will specify the properties or structure of each document in a collection.
+  // These Date objects will be automatically created by the program to store timestamps 
+  // that show the exact time when a user is registered or when their information is modified.
+  created: {
+    type: Date,
+    default: Date.now,
+  },
+  updated: Date,
+});
 
 export default mongoose.model("User", UserSchema); // so that it can be used by the rest of the backend code.
