48577756a72282c290d344d4063fa55c1c5df911
JohnOlutubo <107564128+JohnOlutubo@users.noreply.github.com>
2023-02-18 21:00:39 +0100

    update user.model.js to handle the password sting as a virtual field

--- server/models/user.model.js
+++ server/models/user.model.js
@@ -29,6 +29,25 @@ const UserSchema = new mongoose.Schema({
   Salt: String
 });
 
+// Define a virtual property called 'password' for the user schema
+UserSchema.virtual('password')
+
+  // Define a setter function for the 'password' property
+  .set(function(password) {
+    // Store the original plain-text password in the '_password' variable
+    this._password = password
+    // Generate a random 'salt' value and store it in the 'salt' variable
+    this.salt = this.makeSalt()
+    // Use the 'password' and 'salt' values to generate a secure, one-way 'hashed_password'
+    // value, and store it in the 'hashed_password' variable
+    this.hashed_password = this.encryptPassword(password)
+  })
+
+  // Define a getter function for the 'password' property
+  .get(function() {
+    // Return the original plain-text password that was entered by the user
+    return this._password
+  })
 
 
 export default mongoose.model("User", UserSchema); // so that it can be used by the rest of the backend code.
