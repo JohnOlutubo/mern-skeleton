c6dc9d74c870957739af9d1ef69934fab4f1df03
JohnOlutubo <107564128+JohnOlutubo@users.noreply.github.com>
2023-02-20 11:29:11 +0100

    mount user API routes on the express app

--- server/express.js
+++ server/express.js
@@ -5,6 +5,8 @@ import compress from "compression";
 import helmet from "helmet";
 import cors from "cors";
 import Template from "./../template.js";
+import userRoutes from "./routes/user.routes"
+
 
 const app = express();
 /* ... configure express ... */
@@ -14,9 +16,10 @@ app.use(cookieParser());
 app.use(compress());
 app.use(helmet());
 app.use(cors());
+app.use('/', userRoutes)
+
 
 // send template in the response to a GET request for the '/' route.
-
 app.get("/", (req, res) => {
   res.status(200).send(Template());
 });
