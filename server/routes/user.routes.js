9436d7cb53afd809c8058c8520fd0aa367235e9a
JohnOlutubo <107564128+JohnOlutubo@users.noreply.github.com>
2023-02-21 11:41:26 +0100

    define user routes

--- server/routes/user.routes.js
+++ server/routes/user.routes.js
@@ -1,1 +1,18 @@
 // the Express app's user API endpoints enable CRUD operations on user data. To implement these endpoints, the routes and corresponding controllers are defined.
+
+import express from "express";
+import userCtrl from "../controllers/user.controller";
+
+const router = express.Router();
+
+router.route("api/users").get(userCtrl.list).post(userCtrl.create);
+
+router
+  .route("api/users/:userId")
+  .get(userCtrl.read)
+  .put(userCtrl.update)
+  .delete(userCtrl.remove);
+
+router.param("userId", userCtrl.userByID);
+
+export default router;
