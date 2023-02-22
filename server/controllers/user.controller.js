496d0a728366398f2b7c67a0fe1b0d660c480f09
JohnOlutubo <107564128+JohnOlutubo@users.noreply.github.com>
2023-02-22 16:49:15 +0100

    update user.controller.js with the function for creating new user

--- server/controllers/user.controller.js
+++ server/controllers/user.controller.js
@@ -2,15 +2,41 @@
 // in the preceding user route declarations as callbacks to be executed
 // when a route request is received by the server.
 
-import User from '../models/user.model'
-import extend from 'lodash/extend'
-import ErrorHandler from './error.controller'
+import User from "../models/user.model";
+import extend from "lodash/extend";
+import ErrorHandler from "./error.controller";
 
-const create = (req, res, next) => { /* ... */ }
-const list = (req, res) => { /* ... */ }
-const userByID = (req, res, next, id)  => { /* ... */ }
-const read = (req, res) => { /* ... */ }
-const update = (req, res, next) => { /* ... */ }
-const remove = (req, res, next) => { /* ... */ }
 
-export default { create, userByID, read, list, remove, update }
+/* This function creates a new user with the user JSON object that's received in the POST request from the frontend within req.body. */
+const create = async (req, res) => {
+  const user = new User(req.body)
+  try {
+    await user.save()
+    return res.status(200).json({
+      message: "Successfully signed up!"
+    })
+  } catch (err) {
+    return res.status(400).json({
+    error: errorHandler.getErrorMessage(err)
+    })
+  }
+};
+
+
+const list = (req, res) => {
+  /* ... */
+};
+const userByID = (req, res, next, id) => {
+  /* ... */
+};
+const read = (req, res) => {
+  /* ... */
+};
+const update = (req, res, next) => {
+  /* ... */
+};
+const remove = (req, res, next) => {
+  /* ... */
+};
+
+export default { create, userByID, read, list, remove, update };
