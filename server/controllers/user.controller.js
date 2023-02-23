b40228b0694b1d21cc7fcb48ed9e884745f3c199
JohnOlutubo <107564128+JohnOlutubo@users.noreply.github.com>
2023-02-23 13:30:34 +0100

    update user.controller.js with the function for listing all users

--- server/controllers/user.controller.js
+++ server/controllers/user.controller.js
@@ -23,9 +23,21 @@ const create = async (req, res) => {
 };
 
 
-const list = (req, res) => {
-  /* ... */
+const list = async(req, res) => {
+  try { // This line finds all users in the database and selects specific fields to include in the results.
+    let users = await User.find().select("name email updated created");
+
+    //This line sends a response back to the user with the list of users in the database.
+    res.json(users);
+  } catch (err) {
+    // If there is an error listing the users, the block of code runs.
+    // This line sends a respond back to the user with a status code of 400 (Bad request) and error message.
+    return res.status(400).json({
+      error: errorHandler.getErrorMessage(err)
+    });
+  }
 };
+
 const userByID = (req, res, next, id) => {
   /* ... */
 };
