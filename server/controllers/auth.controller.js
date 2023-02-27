2e61092ffcb1050f14b9ccb706595b5cc1aa8583
JohnOlutubo <107564128+JohnOlutubo@users.noreply.github.com>
2023-02-27 15:00:46 +0100

    create auth.controller.js file to handle requests to the signin and signout routes provide JWT and express-jwt functionality to enable authentication and authorization for protected user API endpoints

---
+++ server/controllers/auth.controller.js
@@ -0,0 +1,12 @@
+import user from "../models/user.model";
+import jwt from 'jsonwebtoken';
+import expressJwt from 'express-jwt';
+import config from './../../config/config.js';
+
+const signin = (req, res) => { /*  */ };
+const signout = (req, res) => { /*  */ };
+const requireSignin = /*  */;
+const hasAuthourization = (req, res) => { /* */ };
+
+
+export default { signin, signout, requireSignin, hasAuthourization }
