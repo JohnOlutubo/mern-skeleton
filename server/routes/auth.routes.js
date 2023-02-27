09df97d6d5f79666d7089638d3db8026b0064754
JohnOlutubo <107564128+JohnOlutubo@users.noreply.github.com>
2023-02-27 15:07:28 +0100

    amend request method for signout

--- server/routes/auth.routes.js
+++ server/routes/auth.routes.js
@@ -3,7 +3,7 @@ import authCtrl from "../controllers/auth.controller";
 
 const router = express.Router();
 
-router.route("/auth/signin").post(authCtrl.signin);
-router.route("/auth/signout").post(authCtrl.signout);
+router.route("/auth/signin").post(authCtrl.signin); // When the Express app gets a POST request at '/auth/signin', it executes the signin controller function.
+router.route("/auth/signout").get(authCtrl.signout);
 
 export default router;
