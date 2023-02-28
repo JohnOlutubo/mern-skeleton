dbe80f8c912763a94c75f2583bd7d0d2c5b88c1c
JohnOlutubo <107564128+JohnOlutubo@users.noreply.github.com>
2023-02-28 11:06:58 +0100

    update auth.controller.js with the controller function for signing out a user

--- server/controllers/auth.controller.js
+++ server/controllers/auth.controller.js
@@ -1,39 +1,51 @@
 import User from "../models/user.model";
-import jwt from 'jsonwebtoken';
-import expressJwt from 'express-jwt';
-import config from './../../config/config.js';
+import jwt from "jsonwebtoken";
+import expressJwt from "express-jwt";
+import config from "./../../config/config.js";
 
 const signin = async (req, res) => {
   try {
-    let user = await User.findOne({ "email": req.body.email });
+    let user = await User.findOne({ email: req.body.email }); // find a user in the database by email address
     if (!user)
+      // if the user doesn't exist, return an error message
       return res.status("401").json({ error: "user not found" });
 
     if (!user.authenticate(req.body.password)) {
-      return res.status('401').json({ error: "Email and password don't match."});
+      // if the user exists, but the password doesn't match, return an error message
+      return res
+        .status("401")
+        .json({ error: "Email and password don't match." });
     }
-    const token = jwt.sign({ _id: user._id }, config.jwtSecret)
 
-    res.cookie('t', token, { expire: new Date() + 9999 } )
+    const token = jwt.sign({ _id: user._id }, config.jwtSecret); // create a token using the user's ID and a secret key
+
+    res.cookie("t", token, { expire: new Date() + 9999 }); // set a cookie in the user's browser with the token, so they stay logged in
 
     return res.json({
+      // return the token and some information about the user (like their name and email) as a JSON object
       token,
       user: {
         _id: user._id,
         name: user.name,
-        email: user.email
-      }
-    })
+        email: user.email,
+      },
+    });
   } catch (err) {
-    return res.status('401').json({ error: "Could not sign in" })
+    return res.status("401").json({ error: "Could not sign in" });
   }
 };
 
-const signout = (req, res) => { /*  */ };
+const signout = (req, res) => {
+  res.clearCookie("t");
+  return res.status("200").json({
+    message: "signed out",
+  });
+};
 
-const requireSignin = /*  */;
+// const requireSignin = ;
 
-const hasAuthourization = (req, res) => { /* */ };
+const hasAuthourization = (req, res) => {
+  /* */
+};
 
-
-export default { signin, signout, requireSignin, hasAuthourization }
+export default { signin, signout, requireSignin, hasAuthourization };
