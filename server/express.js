70a70e0458212a8c298550e46c4ce72dca927baa
JohnOlutubo <107564128+JohnOlutubo@users.noreply.github.com>
2023-02-28 20:14:57 +0100

    update express.js to handle auth-related errors thrown by express-jwt when it tries to validate JWT tokens in incoming requests

--- server/express.js
+++ server/express.js
@@ -5,8 +5,8 @@ import compress from "compression";
 import helmet from "helmet";
 import cors from "cors";
 import Template from "./../template.js";
-import userRoutes from "./routes/user.routes"
-import authRoutes from "./routes/auth.routes"
+import userRoutes from "./routes/user.routes";
+import authRoutes from "./routes/auth.routes";
 
 const app = express();
 /* ... configure express ... */
@@ -16,12 +16,20 @@ app.use(cookieParser());
 app.use(compress());
 app.use(helmet());
 app.use(cors());
-app.use('/', userRoutes)
-app.use('/', authRoutes)
+app.use("/", userRoutes);
+app.use("/", authRoutes);
 
 // send template in the response to a GET request for the '/' route.
 app.get("/", (req, res) => {
   res.status(200).send(Template());
 });
 
+app.use((err, req, res, next) => {
+  if (err.name === "UnauthorizedError") {
+    res.status(401).json({ error: err.name + ": " + err.message });
+  } else if (err) {
+    res.status(400).json({ error: err.name + ": " + err.message });
+    console.log(err);
+  }
+});
 export default app;
